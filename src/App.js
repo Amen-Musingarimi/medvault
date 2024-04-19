6e4028a9410a721f83fc02e8fc2e8e648926ac0c
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-04-19 11:46:41 +0200

    Update the app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>The MedVault App</h1>
+      <h1>The MedVault App: The Doc Assistance App.</h1>
     </div>
   );
 }
