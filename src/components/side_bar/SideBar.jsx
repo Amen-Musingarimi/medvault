04354b473e1bca3a2467e0abc3ca25a3ef99743d
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-04-22 13:09:26 +0200

    Ass a basic structue for the side nav

--- src/components/side_bar/SideBar.jsx
+++ src/components/side_bar/SideBar.jsx
@@ -1,30 +1,16 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { NavLink, Link, useNavigate } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import './SideBar.css';
 import { FaFacebook, FaTwitter, FaPinterestP } from 'react-icons/fa';
-import logo from '../assets/hotelAppLogo.png';
-import { getLocalStorage } from '../helpers/localStorage';
-import {
-  logOutUser,
-  toLogin,
-  toRegister,
-} from '../redux/auth/authenticationSlice';
+import logo from '../../assets/medi_vault_logo.png';
 import { toggleNav } from '../redux/sideBar/sideBarSlice';
 
 const SideBar = () => {
   const dispatch = useDispatch();
   const isAuthenticated = useSelector((state) => state.auth.token !== null);
 
-  const user = getLocalStorage('user');
-
   const isOpen = useSelector((state) => state.nav.isOpen);
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    dispatch(logOutUser());
-    navigate('/');
-  };
 
   const handleHideNav = () => {
     dispatch(toggleNav());
@@ -51,107 +37,92 @@ const SideBar = () => {
         <img src={logo} alt="app-logo" className="app-logo" />
       </div>
       <ul className="pages-link">
-        {isAuthenticated && (
+        <li>
+          <NavLink to="/home" activeclassname="active">
+            <button type="button" onClick={() => handleHideNav()}>
+              HOTELS
+            </button>
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink to="/reservations/my-reservations" activeclassname="active">
+            <button type="button" onClick={() => handleHideNav()}>
+              RESERVATIONS
+            </button>
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink to="/reservations/new" activeclassname="active">
+            <button type="button" onClick={() => handleHideNav()}>
+              RESERVE A ROOM
+            </button>
+          </NavLink>
+        </li>
+
+        <>
           <li>
-            <NavLink to="/home" activeclassname="active">
+            <NavLink to="/hotel/new" activeclassname="active">
               <button type="button" onClick={() => handleHideNav()}>
-                HOTELS
+                ADD HOTEL
               </button>
             </NavLink>
           </li>
-        )}
-        {isAuthenticated && (
           <li>
             <NavLink
-              to="/reservations/my-reservations"
+              to="/hotels"
+              className="last-nav-link"
               activeclassname="active"
             >
               <button type="button" onClick={() => handleHideNav()}>
-                RESERVATIONS
+                DELETE HOTEL
               </button>
             </NavLink>
           </li>
-        )}
-        {isAuthenticated && (
-          <li>
-            <NavLink to="/reservations/new" activeclassname="active">
-              <button type="button" onClick={() => handleHideNav()}>
-                RESERVE A ROOM
-              </button>
-            </NavLink>
-          </li>
-        )}
-        {isAuthenticated && (
-          <>
-            <li>
-              <NavLink to="/hotel/new" activeclassname="active">
-                <button type="button" onClick={() => handleHideNav()}>
-                  ADD HOTEL
-                </button>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/hotels"
-                className="last-nav-link"
-                activeclassname="active"
-              >
-                <button type="button" onClick={() => handleHideNav()}>
-                  DELETE HOTEL
-                </button>
-              </NavLink>
-            </li>
-          </>
-        )}
+        </>
       </ul>
 
       <section className="auth-wrapper">
         <ul className="auth-box">
-          {!isAuthenticated && (
-            <>
-              <li>
-                <Link to="/auth" className="auth-link">
-                  <button
-                    type="button"
-                    onClick={() => {
-                      dispatch(toLogin());
-                      handleHideNav();
-                    }}
-                  >
-                    Log In
-                  </button>
-                </Link>
-              </li>
-              <li>
-                <Link to="/auth" className="auth-link">
-                  <button
-                    type="button"
-                    onClick={() => {
-                      dispatch(toRegister());
-                      handleHideNav();
-                    }}
-                  >
-                    Sign Up
-                  </button>
-                </Link>
-              </li>
-            </>
-          )}
-          {isAuthenticated && user && (
+          <>
             <li>
-              <h4 className="username">{user.name}</h4>
-              <button
-                type="button"
-                className="down-btn"
-                onClick={() => {
-                  handleLogout();
-                  handleHideNav();
-                }}
-              >
-                Log Out
-              </button>
+              <Link to="/auth" className="auth-link">
+                <button
+                  type="button"
+                  onClick={() => {
+                    handleHideNav();
+                  }}
+                >
+                  Log In
+                </button>
+              </Link>
             </li>
-          )}
+            <li>
+              <Link to="/auth" className="auth-link">
+                <button
+                  type="button"
+                  onClick={() => {
+                    handleHideNav();
+                  }}
+                >
+                  Sign Up
+                </button>
+              </Link>
+            </li>
+          </>
+          <li>
+            <h4 className="username">Doc Amen Musinga</h4>
+            <button
+              type="button"
+              className="down-btn"
+              onClick={() => {
+                handleHideNav();
+              }}
+            >
+              Log Out
+            </button>
+          </li>
         </ul>
       </section>
 
