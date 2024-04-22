2f26cb967fa0b181c9c4a0866c2dbcbc82975729
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-04-22 21:51:58 +0200

    Add header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import SideBar from './components/sideBar/SideBar';
+import Header from './components/header/Header';
 import Authentication from './components/auth/Authentication';
 import './App.css';
 
@@ -8,6 +9,7 @@ const App = () => {
   return (
     <div className="main-container">
       <SideBar />
+      <Header />
       <Routes>
         <Route exact path="/auth" element={<Authentication />} />
       </Routes>
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import { IoNotifications } from 'react-icons/io5';
+import classes from './Header.module.css';
+
+const Header = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    // You can perform your search logic here with the searchTerm state
+    console.log('Search term:', searchTerm);
+  };
+
+  return (
+    <div className={classes.header}>
+      <form onSubmit={handleSearchSubmit}>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={handleSearchChange}
+          placeholder="Search..."
+        />
+        <button type="submit">
+          <FaSearch />
+        </button>
+      </form>
+      <IoNotifications />
+    </div>
+  );
+};
+
+export default Header;
