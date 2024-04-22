33f91bda4bba1f321f9ef9bf2e27e2ed4e5622e6
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-04-22 22:43:13 +0200

    Style the notififation bar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -29,7 +29,7 @@ const Header = () => {
           Search
         </button>
       </form>
-      <button type="submit">
+      <button type="submit" className={classes.notification_btn}>
         <IoNotifications />
       </button>
     </div>
