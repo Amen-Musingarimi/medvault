96bacd9f6c1ccb53d78af44112fb6e4bbbcaaae3
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-04-22 21:13:04 +0200

    Add links to the side bar

--- src/components/sideBar/SideBar.jsx
+++ src/components/sideBar/SideBar.jsx
@@ -3,17 +3,9 @@ import classes from './SideBar.module.css';
 import { Link } from 'react-router-dom';
 import { AiFillHome } from 'react-icons/ai';
 import { SlCalender } from 'react-icons/sl';
-import {
-  BsFillTrophyFill,
-  BsFileEarmarkCheckFill,
-  BsFillCalendarDateFill,
-} from 'react-icons/bs';
-import { AiFillSchedule, AiFillQuestionCircle } from 'react-icons/ai';
+import { AiFillSchedule } from 'react-icons/ai';
 import { FaUsers, FaUsersCog, FaUserCog } from 'react-icons/fa';
-import { FcInvite } from 'react-icons/fc';
-import { MdAnnouncement, MdReportProblem } from 'react-icons/md';
 import { FcStatistics } from 'react-icons/fc';
-import { TbAdjustmentsCheck } from 'react-icons/tb';
 import { BiSolidLogOut } from 'react-icons/bi';
 import logo from '../../assets/madvault-logo.png';
 
@@ -30,114 +22,42 @@ const SideBar = () => {
           </Link>
         </div>
         <div className={classes.homeLinkCont}>
-          <p className={classes.linkHeading}>MATCHES</p>
+          <p className={classes.linkHeading}>PATIENTS</p>
           <Link className={classes.homeLink}>
             <SlCalender />
-            <p className={classes.homeText}>Fixtures</p>
+            <p className={classes.homeText}>Manage Patience</p>
           </Link>
           <Link className={classes.homeLink}>
             <AiFillSchedule />
-            <p className={classes.homeText}>Results</p>
-          </Link>
-          <Link className={classes.homeLink}>
-            <BsFillTrophyFill />
-            <p className={classes.homeText}>Standings</p>
+            <p className={classes.homeText}>Add Patient</p>
           </Link>
         </div>
         <div className={classes.homeLinkCont}>
-          <p className={classes.linkHeading}>CLUBS</p>
+          <p className={classes.linkHeading}>APPOINTMENTS</p>
+          <Link className={classes.homeLink}>
+            <FaUsersCog />
+            <p className={classes.homeText}>Set Availability</p>
+          </Link>
           <Link className={classes.homeLink}>
             <FaUsers />
-            <p className={classes.homeText}>Teams</p>
+            <p className={classes.homeText}>Upcoming Appointments</p>
           </Link>
           <Link className={classes.homeLink}>
             <FaUsersCog />
-            <p className={classes.homeText}>Team Sheets</p>
+            <p className={classes.homeText}>Schedule Appointment</p>
           </Link>
         </div>
         <div className={classes.homeLinkCont}>
-          <p className={classes.linkHeading}>GET HELP</p>
-          <Link className={classes.homeLink}>
-            <AiFillQuestionCircle />
-            <p className={classes.homeText}>FAQS</p>
-          </Link>
-          <Link className={classes.homeLink}>
-            <MdAnnouncement />
-            <p className={classes.homeText}>Anouncements</p>
-          </Link>
-          <Link className={classes.homeLink}>
-            <MdReportProblem />
-            <p className={classes.homeText}>Report Misconduct</p>
-          </Link>
-        </div>
-        <div className={classes.homeLinkCont}>
-          <p className={classes.linkHeading}>TEAM ADMIN</p>
-          <Link className={classes.homeLink}>
-            <BsFileEarmarkCheckFill />
-            <p className={classes.homeText}>Submit Team Sheet</p>
-          </Link>
-          <Link className={classes.homeLink}>
-            <FcInvite />
-            <p className={classes.homeText}>Invite Team Members</p>
-          </Link>
-          <Link className={classes.homeLink}>
-            <FaUsersCog />
-            <p className={classes.homeText}>Manage Team</p>
-          </Link>
-          <Link className={classes.homeLink}>
-            <TbAdjustmentsCheck />
-            <p className={classes.homeText}>Manage Fixtures</p>
-          </Link>
-        </div>
-        <div className={classes.homeLinkCont}>
-          <p className={classes.linkHeading}>LEAGUE ADMIN</p>
-          <Link className={classes.homeLink}>
-            <BsFileEarmarkCheckFill />
-            <p className={classes.homeText}>Approve Team Sheet</p>
-          </Link>
-          <Link className={classes.homeLink}>
-            <FcInvite />
-            <p className={classes.homeText}>Invite Team Captains</p>
-          </Link>
-          <Link className={classes.homeLink}>
-            <BsFillCalendarDateFill />
-            <p className={classes.homeText}>Manage Dates</p>
-          </Link>
-          <Link className={classes.homeLink}>
-            <FaUsersCog />
-            <p className={classes.homeText}>Manage Users</p>
-          </Link>
-          <Link className={classes.homeLink}>
-            <TbAdjustmentsCheck />
-            <p className={classes.homeText}>Manage Fixtures</p>
-          </Link>
-        </div>
-        <div className={classes.homeLinkCont}>
-          <p className={classes.linkHeading}>SYSTEM ADMIN</p>
-          <Link className={classes.homeLink}>
-            <FcInvite />
-            <p className={classes.homeText}>Invite League Admins</p>
-          </Link>
-          <Link className={classes.homeLink}>
-            <FaUsersCog />
-            <p className={classes.homeText}>Manage Teams</p>
-          </Link>
-          <Link className={classes.homeLink}>
-            <FaUsersCog />
-            <p className={classes.homeText}>Manage Users</p>
-          </Link>
-        </div>
-        <div className={classes.homeLinkCont}>
-          <p className={classes.linkHeading}>YOUR ACOUNT</p>
+          <p className={classes.linkHeading}>YOUR PROFILE</p>
           <Link className={classes.homeLink}>
             <FaUserCog />
-            <p className={classes.homeText}>Account Details</p>
+            <p className={classes.homeText}>Details</p>
           </Link>
           <Link className={classes.homeLink}>
             <FcStatistics />
-            <p className={classes.homeText}>Game Statistics</p>
+            <p className={classes.homeText}>Settings</p>
           </Link>
-          <Link className={classes.homeLink}>
+          <Link className={`${classes.homeLink} ${classes.danger}`}>
             <BiSolidLogOut />
             <p className={classes.homeText}>Logout</p>
           </Link>
