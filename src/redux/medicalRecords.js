9326cbc961b10c760dd36fba46456f9bab5ff3b5
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-04-30 21:34:25 +0200

    Add medical records redux slice

---
+++ src/redux/medicalRecords.js
@@ -0,0 +1,50 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const initialState = {
+  medicalRecords: [],
+  status: 'idle',
+  error: null,
+};
+
+export const createMedicalReord = createAsyncThunk(
+  'medicalRecords/createMedicalRecords',
+  async (medicalRecordData, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(
+        'http://localhost:8080/medicalRecords/post',
+        medicalRecordData
+      );
+      return response.data.medicalRecord;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+// Define the medical records slice
+const medicalRecordsSlice = createSlice({
+  name: 'medicalRecords',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    // Handle the pending state while creating a patient
+    builder.addCase(createMedicalReord.pending, (state) => {
+      state.status = 'loading';
+    });
+
+    // Handle the success state after creating a patient
+    builder.addCase(createMedicalReord.fulfilled, (state, action) => {
+      state.status = 'succeeded';
+      state.medicalRecords.push(action.payload);
+    });
+
+    // Handle the error state if creating a patient fails
+    builder.addCase(createMedicalReord.rejected, (state, action) => {
+      state.status = 'failed';
+      state.error = action.payload;
+    });
+  },
+});
+
+export default medicalRecordsSlice.reducer;
