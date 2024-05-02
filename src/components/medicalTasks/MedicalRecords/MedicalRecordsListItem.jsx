ef9f2e0d9cdeb5d6fef50e7523b2b06779a0c516
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-05-02 21:17:08 +0200

    Add function to remfommat date

--- src/components/medicalTasks/MedicalRecords/MedicalRecordsListItem.jsx
+++ src/components/medicalTasks/MedicalRecords/MedicalRecordsListItem.jsx
@@ -3,9 +3,20 @@ import { Link } from 'react-router-dom';
 
 const MedicalRecordsListItem = ({ medicalRecord }) => {
   console.log(medicalRecord);
+  function formatDate(dateString) {
+    const options = {
+      weekday: 'long',
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+    };
+    const date = new Date(dateString);
+    return date.toLocaleDateString('en-US', options);
+  }
+
   return (
     <div>
-      <h3>{medicalRecord.createdAt}</h3>
+      <h3>{formatDate(medicalRecord.createdAt)}</h3>
       <p>{medicalRecord.diagnosis}</p>
       <Link>See Details</Link>
     </div>
