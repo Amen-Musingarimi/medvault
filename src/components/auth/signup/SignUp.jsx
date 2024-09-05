8484cc6095d0745155c52801cea87cb686f27d57
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-09-05 12:19:47 +0200

    Add basic layout of the signup form

--- src/components/auth/signup/SignUp.jsx
+++ src/components/auth/signup/SignUp.jsx
@@ -78,114 +78,123 @@ const DoctorSignup = () => {
     <div className="signup-page flex-column">
       <h1>Doctor Signup</h1>
       <form onSubmit={handleSubmit} className="signup-form flex-column">
-        {formError && <div className={classes.error_message}>{formError}</div>}
-        <div>
-          <label>First Name:</label>
-          <input
-            type="text"
-            name="firstName"
-            value={formData.firstName}
-            onChange={handleChange}
-            min={3}
-          />
-          <p className={classes.errorMessage}></p>
-        </div>
-        <div>
-          <label>Last Name:</label>
-          <input
-            type="text"
-            name="lastName"
-            value={formData.lastName}
-            onChange={handleChange}
-            min={3}
-          />
-          <p className={classes.errorMessage}></p>
-        </div>
-        <div>
-          <label>Email Address:</label>
-          <input
-            type="email"
-            name="emailAddress"
-            value={formData.emailAddress}
-            onChange={handleChange}
-          />
-          <p className={classes.errorMessage}></p>
-        </div>
-        <div>
-          <label>Gender:</label>
-          <select name="gender" value={formData.gender} onChange={handleChange}>
-            <option value="">Select Gender</option>
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-          </select>
-          <p className={classes.errorMessage}></p>
-        </div>
-        <div>
-          <label>Phone Number:</label>
-          <input
-            type="text"
-            name="phoneNumber"
-            value={formData.phoneNumber}
-            onChange={handleChange}
-            min={10}
-          />
-          <p className={classes.errorMessage}></p>
-        </div>
-        <div>
-          <label>Specialization:</label>
+        <div className="form-sections">
+          {formError && (
+            <div className={classes.error_message}>{formError}</div>
+          )}
+          <div className="form-control">
+            <label>First Name:</label>
+            <input
+              type="text"
+              name="firstName"
+              value={formData.firstName}
+              onChange={handleChange}
+              min={3}
+            />
+            <p className={classes.errorMessage}></p>
+          </div>
+          <div className="form-control">
+            <label>Last Name:</label>
+            <input
+              type="text"
+              name="lastName"
+              value={formData.lastName}
+              onChange={handleChange}
+              min={3}
+            />
+            <p className={classes.errorMessage}></p>
+          </div>
 
-          <select
-            name="specialization"
-            value={formData.specialization}
-            onChange={handleChange}
-          >
-            <option value="">Select Specialization</option>
-            {specializations.map((specialization) => (
-              <option value={specialization} key={specialization}>
-                {specialization}
-              </option>
-            ))}
-          </select>
-          <p className={classes.errorMessage}></p>
-        </div>
-        <div>
-          <label>Medical License Number:</label>
-          <input
-            type="text"
-            name="medicalLicenseNumber"
-            value={formData.medicalLicenseNumber}
-            onChange={handleChange}
-          />
-          <p className={classes.errorMessage}></p>
-        </div>
-        <div>
-          <label>Hospital:</label>
-          <input
-            type="text"
-            name="hospital"
-            value={formData.hospital}
-            onChange={handleChange}
-          />
-          <p className={classes.errorMessage}></p>
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-          />
-          <p className={classes.errorMessage}></p>
-        </div>
-        <div>
-          <label>Confirm Password:</label>
-          <input
-            type="password"
-            name="confirmPassword"
-            value={formData.confirmPassword}
-            onChange={handleChange}
-          />
+          <div className="form-control">
+            <label>Email Address:</label>
+            <input
+              type="email"
+              name="emailAddress"
+              value={formData.emailAddress}
+              onChange={handleChange}
+            />
+            <p className={classes.errorMessage}></p>
+          </div>
+          <div className="form-control">
+            <label>Gender:</label>
+            <select
+              name="gender"
+              value={formData.gender}
+              onChange={handleChange}
+            >
+              <option value="">Select Gender</option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+            </select>
+            <p className={classes.errorMessage}></p>
+          </div>
+          <div className="form-control">
+            <label>Phone Number:</label>
+            <input
+              type="text"
+              name="phoneNumber"
+              value={formData.phoneNumber}
+              onChange={handleChange}
+              min={10}
+            />
+            <p className={classes.errorMessage}></p>
+          </div>
+          <div className="form-control">
+            <label>Specialization:</label>
+
+            <select
+              name="specialization"
+              value={formData.specialization}
+              onChange={handleChange}
+            >
+              <option value="">Select Specialization</option>
+              {specializations.map((specialization) => (
+                <option value={specialization} key={specialization}>
+                  {specialization}
+                </option>
+              ))}
+            </select>
+            <p className={classes.errorMessage}></p>
+          </div>
+          <div className="form-control">
+            <label>Medical License Number:</label>
+            <input
+              type="text"
+              name="medicalLicenseNumber"
+              value={formData.medicalLicenseNumber}
+              onChange={handleChange}
+            />
+            <p className={classes.errorMessage}></p>
+          </div>
+          <div className="form-control">
+            <label>Hospital:</label>
+            <input
+              type="text"
+              name="hospital"
+              value={formData.hospital}
+              onChange={handleChange}
+            />
+            <p className={classes.errorMessage}></p>
+          </div>
+          <div className="form-control">
+            <label>Password:</label>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+            <p className={classes.errorMessage}></p>
+          </div>
+          <div className="form-control">
+            <label>Confirm Password:</label>
+            <input
+              type="password"
+              name="confirmPassword"
+              value={formData.confirmPassword}
+              onChange={handleChange}
+            />
+          </div>
         </div>
         <button type="submit">Sign Up</button>
       </form>
